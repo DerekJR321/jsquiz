758643b9c5c1186b301d96208f86afb3de747535
Derek Reidy <50381777+DerekJR321@users.noreply.github.com>
2020-01-04 16:20:49 -0500

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,142 @@
+const myQuestions = [
+    {
+        id: 1,
+        title: "Back to the Future",
+        question: "Christopher Lloyd stated that he always wanted to do one more movie, in which Marty and Doc Brown travel back to which time period?",
+        background: "image/bg_slide1.jpg",
+        answers: {
+            a: "Dinosaur Age",
+            b: "Ancient Rome",
+            c: "The Civil War Era"
+        },
+        correctAnswer: "b",
+        correctAnswerText: "Correct! Christopher Lloyd was always interested in traveling back to Rome. In the TV show (Season 1, Episode 5) titled 'Roman Holiday', they did just that!",
+        falseAnswerText: "Incorrect. The correct answer is 'B', Ancient Rome."
+    },
+    {
+        id: 2,
+        title: "Back to the Future",
+        question: "Which character cuts in on George and Lorraine at the Enchantment under the Sea dance?",
+        background: "image/bg_slide2.jpg",
+        answers: {
+            a: "Darren",
+            b: "Dave",
+            c: "Dixon"
+        },
+        correctAnswer: "c",
+        correctAnswerText: "Amazing! Mark Dixon was considered by some as a bully. During the dance, Dixon, possibly drunk from the punch Biff and his gang spiked, cut in during George and Lorraine's slow dance.",
+        falseAnswerText: "Sorry, the correct answer was 'C', Dixon, or 'Mark Dixon'."
+    },
+    {
+        id: 3,
+        title: "Back to the Future",
+        question: "Two instructions appeared on the front on the flux capacitor: 'Disconnect capacitor drive before opening' and ________?",
+        background: "image/bg_slide3.jpg",
+        answers: {
+            a: "Shield Eyes From Light",
+            b: "Refuel When Empty",
+            c: "Caution!"
+        },
+        correctAnswer: "a",
+        correctAnswerText: "Correct! The flux capacitor bore two red Dymo labels, warning 'DISCONNECT CAPACITOR DRIVE BEFORE OPENING' at the top and 'SHIELD EYES FROM LIGHT' across the glass plate.",
+        falseAnswerText: "Incorrect. The correct answer is 'C', Shield Eyes from Light."
+    },
+    {
+        id: 4,
+        title: "Back to the Future",
+        question: "What two songs by Huey Lewis and the News were featured in Back to the Future? 'The Power of Love' and ________?",
+        background: "image/bg_slide4.jpg",
+        answers: {
+            a: "Earth Angel",
+            b: "I Can't Drive 55",
+            c: "Back in Time"
+        },
+        correctAnswer: "c",
+        correctAnswerText: "Correct! 'Back in Time' is the closing song in the movie. It can also be heard playing on his clock radio when Marty wakes up from what he believes is a nightmare.",
+        falseAnswerText: "Sorry, the correct answer is 'C', 'Back in Time'."
+    },
+    {
+        id: 5,
+        title: "Back to the Future",
+        question: "What does Doc pack into his suitcase in case they don't have it in the future?",
+        background: "image/bg_slide5.jpg",
+        answers: {
+            a: "Cotton Underwear",
+            b: "A Revolver",
+            c: "Extra Plutonium"
+        },
+        correctAnswer: "a",
+        correctAnswerText: "Correct. Doc Brown wants to take cotton underwear to the future because he's allergic to all synthetics.",
+        falseAnswerText: "Sorry, the correct answer is 'A', Cotton Underwear, as Doc Brown is allergic to all synthetics."
+    },
+    {
+        id: 6,
+        title: "Back to the Future II",
+        question: "What was the date George McFly was murdered in the alternate 1985?",
+        background: "image/bg_slide6.jpg",
+        answers: {
+            a: "March 17, 1974",
+            b: "March 15, 1973",
+            c: "March 21, 1971"
+        },
+        correctAnswer: "b",
+        correctAnswerText: "Correct! The paper read 'George McFly, local author and professor, was shot dead last night en route to an award dinner by the Hill Valley Civic Committee.'",
+        falseAnswerText: "Nope. The correct answer is 'B', March 15, 1973."
+    },
+    {
+        id: 7,
+        title: "Back to the Future II",
+        question: "Which character was played by a member of the Red Hot Chili Peppers in Back to the Future II and III?",
+        background: "image/bg_slide7.jpg",
+        answers: {
+            a: "David Needling",
+            b: "Douglas Needles",
+            c: "Darren Needle"
+        },
+        correctAnswer: "b",
+        correctAnswerText: "Correct! Douglas J. Needles was played by Michael Balzary, better known as Flea, the lead bassist in Red Hot Chili Peppers.",
+        falseAnswerText: "Nope. The correct answer is 'B', Douglas Needles, played by Flea of the Red Hot Chili Peppers."
+    },
+    {
+        id: 8,
+        title: "Back to the Future II",
+        question: "In Back to the Future II, who sells Marty a 'Pepsi Perfect' in the Cafe 80s?",
+        background: "image/bg_slide8.jpg",
+        answers: {
+            a: "Richard Nixon / Jimmy Carter",
+            b: "Max Headroom / Michael Jackson",
+            c: "Ronald Regan / Ayatollah Khomeini"
+        },
+        correctAnswer: "c",
+        correctAnswerText: "Correct! A digital Ronald Regan and Khomeini were arguing about what food Marty should order. He just wanted a Pepsi.",
+        falseAnswerText: "Sorry, but the correct answer is 'C', Regan and Khomeini argued non-stop about what Marty should order."
+    },
+    {
+        id: 9,
+        title: "Back to the Future II",
+        question: "What was the football game that future Biff played for 1955 Biff?",
+        background: "image/bg_slide9.jpg",
+        answers: {
+            a: "UCLA vs Washington",
+            b: "Army vs Navy",
+            c: "UCLA vs Texas A&amp;M"
+        },
+        correctAnswer: "a",
+        correctAnswerText: "Amazing! The football scores Biff listens to are the actual calls from the November 12, 1955 game.",
+        falseAnswerText: "This was a hard one, but the answer was 'A', UCLA vs Washington."
+    },
+    {
+        id: 10,
+        title: "Back to the Future II",
+        question: "What is the date and time at which Doc, Marty and Jennifer arive in 2015?",
+        background: "image/bg_slide10.jpg",
+        answers: {
+            a: "November 5, 2015 at 3:30pm",
+            b: "October 31, 2015 at 4:09pm",
+            c: "October 21, 2015 at 4:29pm"
+        },
+        correctAnswer: "c",
+        correctAnswerText: "Correct! While in the DeLorean, Marty asks Doc where they are and Doc tells him the precise date and time at which they arrived in 2015.",
+        falseAnswerText: "Wrong Butthead! The correct answer is 'C'."
+    }
+]
